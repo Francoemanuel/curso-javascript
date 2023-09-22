7adfec7c32cddee645c51bdf072e908405112b23
Francoemanuel <francoemanuel039@gmail.com>
2023-09-22 06:59:23 -0300

    cambiador de temas

---
+++ proyecto-js/js/main.js
@@ -0,0 +1,66 @@
+
+$(document).ready(function(){
+    // Slider
+    $('.bxslider').bxSlider({
+      mode: 'fade',
+      captions: true,
+      slideWidth: 1200,
+      responsive: true,
+      pager: true
+    });
+    // Posts
+    var posts = [
+        {
+            title: 'Prueba de titulo 1',
+            date: 'Publicado el día ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptatum quos dolores dolorum eum voluptates corporis necessitatibus, quaerat reiciendis dolor excepturi impedit explicabo dignissimos. Perspiciatis ullam voluptatibus quas impedit ab!'
+        },
+        {         
+            title: 'Prueba de titulo 2',
+            date: 'Publicado el día ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptatum quos dolores dolorum eum voluptates corporis necessitatibus, quaerat reiciendis dolor excepturi impedit explicabo dignissimos. Perspiciatis ullam voluptatibus quas impedit ab!'
+        },
+        {
+            title: 'Prueba de titulo 3',
+            date: 'Publicado el día ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptatum quos dolores dolorum eum voluptates corporis necessitatibus, quaerat reiciendis dolor excepturi impedit explicabo dignissimos. Perspiciatis ullam voluptatibus quas impedit ab!'
+        },
+        {
+            title: 'Prueba de titulo 4',
+            date: 'Publicado el día ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptatum quos dolores dolorum eum voluptates corporis necessitatibus, quaerat reiciendis dolor excepturi impedit explicabo dignissimos. Perspiciatis ullam voluptatibus quas impedit ab!'
+        },
+        {
+            title: 'Prueba de titulo 5',
+            date: 'Publicado el día ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptatum quos dolores dolorum eum voluptates corporis necessitatibus, quaerat reiciendis dolor excepturi impedit explicabo dignissimos. Perspiciatis ullam voluptatibus quas impedit ab!'
+        },
+    ];
+    posts.forEach((item, index) => {
+        // var post es una plantilla de javascript
+        var post = `
+            <article class="post">
+                <h2>${item.title}</h2>
+                <span class="date">${item.date}</span>
+                <p>
+                    ${item.content}
+                </p>
+                <a href="#" class="button-more">Leer más</a>
+            </article>        
+        `;
+        
+        $("#posts").append(post);
+
+        // Selector de tema
+        var theme = $("#theme");
+        $("#to-green").click(function(){
+            theme.attr("href", "css/green.css");
+        });
+        $("#to-red").click(function(){
+            theme.attr("href", "css/red.css");
+        });
+        $("#to-blue").click(function(){
+            theme.attr("href", "css/blue.css");
+        });
+    })
+});
